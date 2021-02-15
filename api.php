7d91dcaca558b1d6e2b5b6e3c75c87bcb6b51ec6
pws_wael <procreagency@gmail.com>
2021-02-15 10:10:46 +0100

    api updates

--- api.php
+++ api.php
@@ -3,7 +3,7 @@ include("woocommerce.php");
 $list_product = json_decode(RESTAPI("/wp-json/wc/v3/products", "GET"));
 foreach($list_product as $one){
 	
-	$itemcode=7000017;//;$one['id']; // !!!!!!
+	$itemcode=7000017;//$one['id']; // !!!!!!
 	
 	$ch = curl_init();
 
@@ -11,15 +11,21 @@ foreach($list_product as $one){
 	curl_setopt($ch, CURLOPT_URL, "http://emmam.wingssoft.com/api/getbalbyitem?ItemCode=".$itemcode);
 
 	//return the transfer as a string
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FAILONERROR,1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+  
 
 	// $output contains the output string
 	$output = curl_exec($ch);
-	var_dump($output);
+
 	
 	// close curl resource to free up system resources
 	curl_close($ch);   
+	//header('Content-type: application/xml');
+$movies = new SimpleXMLElement($output);
 
+echo $movies->movie[0]->plot;
 break;	
 }
 ?>
